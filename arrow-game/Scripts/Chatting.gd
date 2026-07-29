e5b524e7f785d7c177ea7b91d0e9d1e3a9c25881
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-07-29 23:14:30 +0900

    채팅의 기본 뼈대 완성

--- arrow-game/Scripts/Chatting.gd
+++ arrow-game/Scripts/Chatting.gd
@@ -1,6 +1,11 @@
-class_name Comment
+extends Control
 
 enum Skill { Newbie, Middleman, Pro, Demon, Ranker }
+
+@export var icon: TextureRect
+@export var chat: Label
+@export var username: Label
+@export var imojiHolders: Array[Control]
 
 var skill: Skill
 var upper_left_arrow: int
